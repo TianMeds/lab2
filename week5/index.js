2ec76b60b07c6aa84432d18f98712773cb2423ea
Christian Medallada <cbmedallada@student.apc.edu.ph>
2023-01-24 11:34:32 +0800

    Update index.js

--- week5/index.js
+++ week5/index.js
@@ -1,14 +1,6 @@
 // Fix Nav
 const navBar = document.querySelector(".nav");
 const navHeight = navBar.getBoundingClientRect().height;
-window.addEventListener("scroll", () => {
-  const scrollHeight = window.pageYOffset;
-  if (scrollHeight > navHeight) {
-    navBar.classList.add("fix-nav");
-  } else {
-    navBar.classList.remove("fix-nav");
-  }
-});
 
 
 // Toggle Menu
