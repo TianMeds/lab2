d3e50db393e63b96a86f7714a6c87c12acf8160a
Christian Medallada <cbmedallada@student.apc.edu.ph>
2023-01-24 13:33:43 +0800

    Lesson 17.5 - Object Display

--- week5/index.js
+++ week5/index.js
@@ -56,6 +56,25 @@ window.addEventListener("scroll", () => {
 
 const glide = document.querySelector(".glide");
 
+if (glide)
+  new Glide(glide, {
+    type: "carousel",
+    startAt: 0,
+    perView: 3,
+    gap: 30,
+    hoverpause: true,
+    autoplay: 2000,
+    animationDuration: 800,
+    animationTimingFunc: "linear",
+    breakpoints: {
+      996: {
+        perView: 2,
+      },
+      768: {
+        perView: 1,
+      },
+    },
+  }).mount();
 
 AOS.init();
 
