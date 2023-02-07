5db3723d944c438487f369e2e0e3ce070f903827
TianMeds <cbmedallada@student.edu.ph>
2023-02-07 20:12:22 +0800

    Added my table

--- week9/guest.php
+++ week9/guest.php
@@ -15,7 +15,7 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "SELECT fullname, email, messages FROM messages";
+$sql = "SELECT fullname, email, messages FROM cbmedallada_MyGuests";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
