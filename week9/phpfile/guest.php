a574597f9cfbc30a5d562c66652b0384ad7eaf93
Christian Medallada <cbmedallada@student.apc.edu.ph>
2023-02-13 09:22:17 +0700

    Update guest.php

--- week9/phpfile/guest.php
+++ week9/phpfile/guest.php
@@ -11,13 +11,13 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "SELECT id, fullname, email, services, messages FROM cbmedallada_MyGuests";
+$sql = "SELECT fullname, email, services, messages FROM cbmedallada_MyGuests";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
   // output data of each row
   while($row = $result->fetch_assoc()) {
-    echo "id: " . $row["id"]. " - Name: " . $row["fullname"]. " - Email:  " . $row["email"]. " - Services: " . $row["services"]. " - Messages: " . $row["messages"]. "<br>";
+    echo "Name: " . $row["fullname"]. " - Email:  " . $row["email"]. " - Services: " . $row["services"]. " - Messages: " . $row["messages"]. "<br>";
   }
 } else {
   echo "0 results";
