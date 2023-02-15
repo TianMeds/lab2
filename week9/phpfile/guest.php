8395320a613db0226adb933e6293da2807f39dea
Christian Medallada <cbmedallada@student.apc.edu.ph>
2023-02-15 19:26:06 +0800

    Update guest.php

--- week9/phpfile/guest.php
+++ week9/phpfile/guest.php
@@ -1,11 +1,11 @@
 <?php
-$servername = "192.168.150.213";
-$username = "webprogmi211";
-$password = "j@zzyAngle30";
-$dbname = "webprogmi211";
-
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+  $servername = "192.168.150.213";
+  $username = "webprogmi211";
+  $password = "j@zzyAngle30";
+  $dbname = "webprogmi211";
+  
+  // Create connection
+  $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
